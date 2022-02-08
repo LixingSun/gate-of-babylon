39db6b0f95e0eacc72dce0f755f44843fc52c564
Lixing <slxtrjh325@gmail.com>
2022-02-08 23:13:02 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Gate of Babylon',
+  tagline: 'My Knowledge Base',
+  url: 'https://lixingsun.github.io',
+  baseUrl: '/gate-of-babylon/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'LixingSun', // Usually your GitHub org/user name.
+  projectName: 'gate-of-babylon', // Usually your repo name.
 
   presets: [
     [
