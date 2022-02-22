61b14af1b034726ddb3c3a9020ba8c80ccbb0b40
LixingSun <LixingSun@users.noreply.github.com>
2022-02-22 16:03:35 +0000

    deploy: 7ef8c95f2a73f4d78de0642e32ebd2cf39d81fdd

---
+++ en/assets/js/d4b6bfb4.fa661c16.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgate_of_babylon=self.webpackChunkgate_of_babylon||[]).push([[7008],{5606:function(e){e.exports=JSON.parse('{"permalink":"/gate-of-babylon/en/game","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
